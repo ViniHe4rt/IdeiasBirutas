0cac6578e6c1fa995af49a8646af6c3fc7129657
AntonioVinicius <antonioviniciussilvamoura@gmail.com>
2020-05-09 17:53:00 -0300

    new idea page + done front

--- Script/script.js
+++ Script/script.js
@@ -1,5 +1,9 @@
 console.log('EIta');
 
 function newIdea(){
-    document.querySelector("#modal").classList.toggle("hide")
+    document.querySelector("#modal").classList.toggle("hide"); 
+
+    document.querySelector("body").classList.toggle("hideScroll");
+
+    document.querySelector("#modal").classList.toggle("addScroll");
 }
